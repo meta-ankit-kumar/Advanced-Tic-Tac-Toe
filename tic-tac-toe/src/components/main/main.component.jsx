69bff25876907093e013868a9a58fb1fb728a290
Ankit Kumar <ankit.kumar@financialforce.com>
2022-06-08 09:23:02 +0530

    TIC-11 Configured and applied changes for eslint

--- tic-tac-toe/src/components/main/main.component.jsx
+++ tic-tac-toe/src/components/main/main.component.jsx
@@ -1,16 +1,16 @@
-import { Game } from "../game/game.component";
-import Loading from "../loading/loading.component";
+import { Game } from '../game/game.component';
+import Loading from '../loading/loading.component';
 
 /**
  * Main component 
  * @param {*} props Properties
  */
-export const Main = (props) => {
-    return (
-        <h1>
+export const Main = () => {
+	return (
+		<h1>
             Tic Tac Toe is coming.... Ankit Bishnoi is heading this project Continue.
-            <Loading/>
-            <Game/>
-        </h1>
-    );
-}
+			<Loading/>
+			<Game/>
+		</h1>
+	);
+};
